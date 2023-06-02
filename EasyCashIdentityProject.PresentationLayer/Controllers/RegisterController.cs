e4cb736db8010c603c8d61ea0c904aee0ecb7188
efekanbay0 <bayefekan1@gmail.com>
2023-06-03 00:26:10 +0300

    Mail Aktivasyon Ekrani

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -62,6 +62,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 					client.Send(mimeMessage);
 					client.Disconnect(true);
 
+					TempData["Mail"] = appUserRegisterDto.Email;
+
 					return RedirectToAction("Index", "ConfirmMail");
 				}
 				else
